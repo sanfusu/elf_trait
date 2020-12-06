7a8cf27fb1457a435492974c8b03a6ea7d24d843
sanfusu <sanfusu@foxmail.com>
2020-12-06 20:17:10 +0800

    add license

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+// Copyright (C) 2020 sanfusu@foxmail.com
+// 
+// This file is part of accessor.
+// 
+// accessor is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// accessor is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with accessor.  If not, see <http://www.gnu.org/licenses/>.
+
 use accessor::*;
 use std::ops::Range;
 // 这里定义的所有 trait 都应该是为了解析而必备的。
